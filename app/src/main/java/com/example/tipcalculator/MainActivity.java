cf974eec8a3b92a68e0ad52cb4c9a6fcd9d76feb
fkemeh18 <35942789+fkemeh18@users.noreply.github.com>
2019-06-06 21:15:20 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.tipcalculator;
-
+//ayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
